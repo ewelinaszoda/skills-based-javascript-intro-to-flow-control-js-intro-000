19a82813df3aae686a25c3d3cabb2fad705e9d0e
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-03-23 20:11:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,12 +26,13 @@ function ternaryTeenager(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager";
   } else {
-    return "You are not a teenager"
+    return "You are not a teenager";
   }
 
 }
 
 function switchAge(age) {
-  switch
-
+  if (age < 13 && age > 19) {
+    return "You have an age";
+  }
 }
